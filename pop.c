37372d7810716dd95c450a8df13572b1e8c303f2
abel mesfin <abelmesfin555@gmail.com>
2022-09-08 09:26:14 +0300

    bug fix

--- pop.c
+++ pop.c
@@ -9,7 +9,7 @@ void pop(stack_t **head, unsigned int l_count)
 {
 	stack_t *rmv;
 
-	if (!head || !*head)
+	if (!head || !(*head))
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", l_count);
 		exit(EXIT_FAILURE);
