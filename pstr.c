cd7c9c246723704f315aaf1a55b9eddb96b5b571
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-09-08 17:05:22 +0300

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -10,13 +10,11 @@ void pstr(stack_t **head, unsigned int l_count __attribute__((unused)))
 {
 	stack_t *current = *head;
 
-	if (!head || !*head)
-		return;
 	while (current)
 	{
 		if (current->n <= 0 || current->n > 127)
 			break;
-		putchar((char)current->n);
+		putchar((char) current->n);
 		current = current->next;
 	}
 	putchar('\n');
