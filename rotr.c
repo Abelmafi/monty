b57113ed7f6d98e6a53f66387bf3ba2163e54a74
abel mesfin <abelmesfin555@gmail.com>
2022-09-08 16:24:49 +0300

    output error fix

--- rotr.c
+++ rotr.c
@@ -7,17 +7,17 @@
  */
 void rotr(stack_t **head, unsigned int l_count __attribute__((unused)))
 {
-	stack_t *temp = NULL, *swap = *head;
+	stack_t *last, *bottom;
 
 	if (!head || !*head || (*head)->next)
 		return;
-	while (swap)
-	{
-		temp = swap->prev;
-		swap->prev = swap->next;
-		swap->next = temp;
-		swap = swap->prev;
-	}
-	if (temp)
-		*head = temp->prev;
+	bottom = *head;
+	while (bottom->next)
+		bottom = bottom->next;
+	last = bottom->prev;
+	last->next = NULL;
+	bottom->prev = NULL;
+	bottom->next = *head;
+	(*head)->prev = bottom;
+	*head = bottom;
 }
