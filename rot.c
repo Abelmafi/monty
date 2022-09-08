4445de6c1972120dd7d72fae9d5da0c051f30ca3
abel mesfin <abelmesfin555@gmail.com>
2022-09-08 17:33:41 +0300

    commited

--- rot.c
+++ rot.c
@@ -10,13 +10,13 @@ void rotl(stack_t **head, unsigned int l_count __attribute__((unused)))
 {
 	stack_t *current = *head, *tmp = *head;
 
-	if (!head || !*head !(*head)->next)
+	if (!head || !*head || !(*head)->next)
 		return;
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = current;
 	*head = (*head)->next;
-	(*head)->prev = NULL:
+	(*head)->prev = NULL;
 	current->prev = tmp;
 	current->next = NULL;
 }
