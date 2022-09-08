a4535f1b6509af1252ad8b0cf8e41a043a6d36c6
abel mesfin <abelmesfin555@gmail.com>
2022-09-08 12:44:12 +0300

    error message requirement error fix

--- pchar.c
+++ pchar.c
@@ -1,16 +1,4 @@
 #include "monty.h"
-#include <ctype.h>
-/**
- * _isalpha - check number if its alpha bet.
- * @c: ascii value input.
- *
- * Return: length of string.
- */
-
-int _isalpha(int c)
-{
-	return (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')));
-}
 /**
  * pchar - prints the char at the top of the stack, followed by a new line.
  * @head: linked list head pointer.
@@ -27,7 +15,7 @@ void pchar(stack_t **head, unsigned int l_count)
 		exit(EXIT_FAILURE);
 	}
 	s = (*head)->n;
-	if (s < 0 || s > 127)
+	if (s < 0 || s > 255)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", l_count);
 		exit(EXIT_FAILURE);
